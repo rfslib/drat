6a0a59ba33192c0eb458520207d0ef0d8df2136b
rfslib <ed.ch@colemanzoo.com>
2023-04-11 17:54:46 -0700

    refactor for functions, prepare for appdata

--- drat.py
+++ drat.py
@@ -9,11 +9,17 @@ purpose: reset equipment computer desktops in the RFSL
     2a. delete all folders not matching ones in the template
     3. copy the correct set of files to the patron desktop
     4. if requested, delete all files in all folders on the desktop
-    5. TODO load the registry with default values for the apps
+    5. TODO load the registry with default values for the apps, and copy AppData config files
     6. TODO recursively clear directories
     7. TODO recursively copy directories (already done?)
     z. log what was done
 '''
+
+# https://pypi.org/project/Send2Trash/, https://www.geeksforgeeks.org/how-to-delete-files-in-python-using-send2trash-module/
+# https://pyinstaller.org/en/stable/operating-mode.html#bundling-to-one-file
+# https://medium.com/swlh/easy-steps-to-create-an-executable-in-python-using-pyinstaller-cc48393bcc64
+# 
+
 
 import platform
 import os
@@ -27,11 +33,50 @@ from send2trash import send2trash
 
 debug = False
 verbose = False # -v on the command line to override
+reset_configs = False # -c on the command line to override
 
 target_base = 'C:\\Users\\'
 source_dir = 'C:\\FSC\\drat\\template\\'
 #source_dir = 'C:\\Users\\rfsl\\rfslib\\drat\\template\\'
 
+#---
+# main starts here
+def main(argv):
+    global verbose, debug, reset_configs
+
+    process_command_line(argv) # set program control args from the command line
+
+    # 0. show some system information
+    user = os.getlogin() # get the current username
+    print(f'This is computer "{platform.node()}" running {platform.system()} {platform.release()} for user "{user}"\n')
+
+    # 1. verify that there is a template for this user
+    source_foo = os.path.join(source_dir, user)
+    target_foo = get_template_dir(user)
+    if target_foo == '':
+        print(f'A template does not exist for the "{user}" user. Aborting...')
+        return()
+    if debug: print(f'Ok, processing stuff in {target_foo}')
+    if debug: print(f'    getting stuff from {source_foo}')
+    
+    # gather the template directory items (directories to be cleaned up)
+    dirs_to_clean = scanDir(source_foo, 'd')
+
+    # clean each directory in the template: delete everything in it and copy back from the template
+    for item in dirs_to_clean:
+        target_dir = os.path.join(target_foo, item.filename)
+        reset_dir(item.path, target_dir)
+
+    # TODO: registry and AppData files
+
+    print('\nREMEMBER TO EMPTY THE RECYCLE BIN!\n'
+            'NOTE: click on the desktop and press F5 if folders are not showing as expected')
+
+
+    return
+
+#---
+# scan a directory and return a list of items in it (f=file or shortcut, d=directory)
 def scanDir(dir, type=''):
     global verbose, debug
     items = []
@@ -44,8 +89,20 @@ def scanDir(dir, type=''):
             items.append(item)
     return(items)
 
-def main(argv):
-    global verbose, debug
+# ---
+# get the template directory for the current user
+def get_template_dir(user):
+    template_dirs = scanDir(source_dir, 'd') # get a list of the user templates
+    for item in template_dirs:
+        if item.filename.lower() == user.lower():
+            return(os.path.join(target_base, user))
+            break
+
+
+# ---
+# process the command line and set control variables as needed
+def process_command_line(argv):
+    global verbose, debug, reset_configs
 
     # process command-line arguments
     if debug: print('Number of arguments:', len(argv), 'arguments.')
@@ -58,45 +115,34 @@ def main(argv):
         elif opt == "-v":
             print('Setting Verbose mode')
             verbose = True
-
-    # ----
+        elif opt == "-c":
+            print('Resetting configuration files')
+            reset_configs = True
     if debug: print(f'verbosity: {verbose}')
-    user = os.getlogin() # get the current username
+    return()
 
-    # 0. show some system information
-    print(f'This is computer "{platform.node()}" running {platform.system()} {platform.release()} for user "{user}"\n')
+# ---
+# copy items from one directory to another
+def copy_dir(expected_items, target_dir):
+    print(f'Restoring {target_dir}')
+    for fn in expected_items:
+        target = os.path.join(target_dir, fn.filename)
+        if fn.filename == 'desktop.ini':
+            if (verbose): print(f'  {fn.filename} - skipped')
+            pass
+        elif fn.type == 'f':
+            if verbose: print(f'  {fn.filename} - file to be copied')
+            shutil.copy(fn.path, target)
+            os.chmod(target, stat.S_IREAD) # set read-only
+        elif fn.type == 'd':
+            if verbose: print(f'  {fn.filename} - folder to be copied')
+            shutil.copytree(fn.path, target)
+            os.chmod(target, stat.S_IREAD) # set read-only
 
-    # 1. verify that this is running as a configured account
-    template_dirs = scanDir(source_dir, 'd') # get a list of the user templates
-    target_foo = ''
-    for item in template_dirs:
-        if item.filename.lower() == user.lower():
-            target_foo = os.path.join(target_base, user)
-            source_foo = os.path.join(source_dir, user)
-            break
-    if target_foo == '':
-        print(f'A template does not exist for the "{user}" user. Aborting...')
-        return()
-    else:
-        if debug: print(f'Ok, processing stuff in {target_foo}')
-        if debug: print(f'    getting stuff from {source_foo}')
 
-
-    # gather the template directory items (directories to be cleaned up, usually Desktop)
-    dirs_to_clean = scanDir(source_foo, 'd')
-
-    for item in dirs_to_clean:
-        target_dir = os.path.join(target_foo, item.filename)
-        clean_dir(item.path, target_dir)
-
-    print('\nREMEMBER TO EMPTY THE RECYCLE BIN!\n'
-            'NOTE: click on the desktop and press F5 if folders are not showing as expected')
-
-    # TODO: registry
-
-    return
-
-def clean_dir(source_dir, target_dir):
+# ---
+# delete everything in the target dir, copy everything from the source dir to the target dir
+def reset_dir(source_dir, target_dir):
     global verbose, debug
     if debug: print(f'Cleaning {target_dir} from {source_dir}')
 
@@ -118,6 +164,9 @@ def clean_dir(source_dir, target_dir):
         if fn.filename == 'desktop.ini':
             if verbose: print(f'  {fn.filename} - skipped')
             pass
+        elif fn.filename.lower() == 'appdata':
+            if verbose: print(f'  {fn.filename} - skipped')
+            pass
         elif fn.type == 'f' or fn.type == 'd':
             if verbose: 
                 if fn.type == 'f': print(f'  {fn.filename} - file to be deleted')
@@ -129,20 +178,7 @@ def clean_dir(source_dir, target_dir):
             if verbose: print(f'  {fn.filename} ok')
 
     # 3. copy the correct set of files to the patron desktop
-    print(f'Restoring {target_dir}')
-    for fn in expected_items:
-        target = os.path.join(target_dir, fn.filename)
-        if fn.filename == 'desktop.ini':
-            if (verbose): print(f'  {fn.filename} - skipped')
-            pass
-        elif fn.type == 'f':
-            if verbose: print(f'  {fn.filename} - file to be copied')
-            shutil.copy(fn.path, target)
-            os.chmod(target, stat.S_IREAD) # set read-only
-        elif fn.type == 'd':
-            if verbose: print(f'  {fn.filename} - folder to be copied')
-            shutil.copytree(fn.path, target)
-            os.chmod(target, stat.S_IREAD) # set read-only
+    copy_dir(expected_items, target_dir)
 
     print()
 
